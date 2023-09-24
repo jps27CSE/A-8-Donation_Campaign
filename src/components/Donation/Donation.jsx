59b6a167749bb24e065751dbe165a6d65da71a52
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-09-25 02:00:34 +0600

    Show all button functionality

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -5,18 +5,32 @@ import DonationCard from "./DonationCard/DonationCard";
 const Donation = () => {
   const [DonationData, setDonationData] = useState([]);
   const [ApplyDonate, setApplyDonate] = useState([]);
+  const [lengthItems, setLengthItems] = useState(4);
+  const [displayData, setDisplayData] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+  const handleShowAll = () => {
+    setLengthItems(ApplyDonate.length);
+    setShowAll(true);
+  };
+
   useEffect(() => {
     fetch("Data.json")
       .then((res) => res.json())
-      .then((data) => setDonationData(data));
+      .then((data) => {
+        setDisplayData(data), setDonationData(data);
+      });
   }, []);
 
   useEffect(() => {
     const storeData = getStoreData();
     if (DonationData.length > 0) {
-      const appliedDonate = DonationData.filter((singleData) =>
-        storeData.includes(singleData.id)
-      );
+      const appliedDonate = [];
+      storeData.forEach((id) => {
+        const matchingDonations = DonationData.filter(
+          (singleData) => singleData.id === id
+        );
+        appliedDonate.push(...matchingDonations);
+      });
       setApplyDonate(appliedDonate);
     }
   }, [DonationData]);
@@ -24,12 +38,19 @@ const Donation = () => {
   return (
     <div className="max-w-6xl mx-auto mt-10 ">
       <div className="grid grid-cols-2">
-        {ApplyDonate.map((donation) => (
+        {ApplyDonate.slice(0, lengthItems).map((donation) => (
           <DonationCard key={donation.id} donation={donation} />
         ))}
       </div>
       <div className="mx-auto flex justify-center">
-        <button className="btn btn-success text-white">Show All</button>
+        {ApplyDonate.length > 4 && !showAll && (
+          <button
+            onClick={handleShowAll}
+            className="btn btn-success text-white mb-5"
+          >
+            Show All
+          </button>
+        )}
       </div>
     </div>
   );
